174860dc4d4ba78930018045429bc0773dd9dc71
Marco Valente <marco.valente@cern.ch>
2022-08-20 01:32:48 +0200

    Updating submission scripts

--- files/job_submission/grid/whizard/submit_grid.py
+++ files/job_submission/grid/whizard/submit_grid.py
@@ -1,28 +1,42 @@
 from subprocess import Popen
 import os
 from datetime import datetime as dt
+from optparse import OptionParser
+from pprint import pprint as p
 
-dry_run=False
+parser = OptionParser('submit_grid [OPTIONS] [inDS]')
+parser.add_option("-d", "--dry-run",  action='store_true', default=False, help="Enable dry-run")
+parser.add_option("-n", "--info",     default='', help="Info for GRID job output name")
+parser.add_option("-s", "--subs-str", default='', help="Substitution string")
 
-info='HH_kl2p5'
+(options, args) = parser.parse_args()
+p('Options:')
+p(vars(options))
+p('Arguments:')
+p(args)
+
 docker_image="docker://whizard/whizard-weekly:master"
 outputs = {
            'lhe':'*.lhe',
-           'out':'*',
+        #    'out':'*',
           }
-singularity_exec = "./whizard.sh -s gh3=1.0/gh3=2.5"
-username=os.environ.get('USER')
-assert username!='', "Username variable $USER is empty"
 
-outDS=f"user.{username}.singularity_whizard.{info}.{dt.now().strftime('%y%m%d_%H%M')}"
+for sin_file in args:
+    singularity_exec = f"./whizard.sh -i {sin_file}"
+    if options.subs_str!='': singularity_exec+=f' -s {options.subs_str}'
 
-command=["prun",
-         "--exec", singularity_exec,
-         "--containerImage", docker_image,
-         "--outputs", ",".join([f'{name}:{out}' for name,out in outputs.items()]),
-         "--outDS", f"{outDS}"]
+    username=os.environ.get('USER')
+    assert username!='', "Username variable $USER is empty"
 
-print(' '.join(command))
-if not dry_run:
-    process=Popen(command)
-    process.wait()
+    outDS=f"user.{username}.singularity_whizard.{options.info}.{dt.now().strftime('%y%m%d_%H%M')}"
+
+    command=["prun",
+            "--exec", singularity_exec,
+            "--containerImage", docker_image,
+            "--outputs", ",".join([f'{name}:{out}' for name,out in outputs.items()]),
+            "--outDS", f"{outDS}"]
+
+    print(' '.join(command))
+    if not options.dry_run:
+        process=Popen(command)
+        process.wait()
---
+++ files/job_submission/grid/whizard/submit_multiple.py
@@ -0,0 +1,8 @@
+import os
+
+os.system('python submit_grid.py cards/mumu_bbbb.sin -n mumu_bbbb')
+os.system('python submit_grid.py cards/mumu_bbuu.sin -n mumu_bbuu')
+os.system('python submit_grid.py cards/mumu_bbdd.sin -n mumu_bbdd')
+os.system('python submit_grid.py cards/mumu_bbcc.sin -n mumu_bbcc')
+os.system('python submit_grid.py cards/mumu_bbss.sin -n mumu_bbss')
+os.system('python submit_grid.py cards/mumu_ttbar.sin -n mumu_ttbar')
